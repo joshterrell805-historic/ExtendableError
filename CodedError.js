d7e2f3665399bd29a6f3cb4af12a4edd8c549336
Josh Terrell <joshterrell805@gmail.com>
2015-02-01 13:20:17 -0800

    Add code to COdedError message

--- CodedError.js
+++ CodedError.js
@@ -10,7 +10,11 @@ if (module) {
 function CodedError(code) {
   ExtendableError.apply(this, Array.prototype.slice.call(arguments, 1));
   this.code = code;
+  this.message = 'CODE: ' + code;
 }
 CodedError.prototype = Object.create(ExtendableError.prototype);
 CodedError.prototype.name = 'CodedError';
 CodedError.prototype.constructor = CodedError;
+CodedError.prototype.toString = function toString() {
+  return 'CodedError: ' + this.message;
+};
